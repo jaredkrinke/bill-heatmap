d88821bf7dc720cf089f3f44307d183fa5d3b468
jaredkrinke <jaredkrinke@gmail.com>
2014-03-30 21:02:50 -0700

    Some initial event handling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
-﻿
+﻿window.onload = function () {
+    var tbody = document.getElementById('billsBody');
+    var editTemplate = document.getElementById('editTemplate');
+    var addHint = document.getElementById('addHint');
+
+    var showEditor = function () {
+        addHint.className = 'hidden';
+        editTemplate.className = 'visible';
+    };
+
+    var hideEditor = function () {
+        addHint.className = null;
+        editTemplate.className = null;
+    };
+
+    // Map from anchor IDs to associated handlers
+    var clickHandlers = [
+        ['add', showEditor],
+        ['editCancel', hideEditor],
+    ];
+
+    // Setup event handling
+    for (var i = 0, count = clickHandlers.length; i < count; i++) {
+        (function (entry) {
+            var id = entry[0];
+            var handler = entry[1];
+            var anchor = document.getElementById(id);
+            anchor.href = '#';
+            anchor.onclick = function () {
+                handler();
+                return false;
+            };
+        })(clickHandlers[i]);
+    }
+};
