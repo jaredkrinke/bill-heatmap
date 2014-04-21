14e375c306166418da5523aac2a3e86b68096b97
jaredkrinke <jaredkrinke@gmail.com>
2014-04-21 08:04:22 -0700

    Noted bug

--- js/main.js
+++ js/main.js
@@ -281,6 +281,7 @@
     }
 
     // Load or create bills list
+    // TODO: On the live site there's a bug when there are no bills at page load
     var billsKey = 'bills';
     var billsJSON = localStorage[billsKey];
     if (billsJSON) {
